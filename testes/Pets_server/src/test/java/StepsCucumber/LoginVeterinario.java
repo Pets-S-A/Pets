bed6b949525d10c5fea4d3e31f367f76876b8b6c
Tereza <terezaarual@hotmail.com>
2020-05-26 21:59:34 -0300

    Login Veterinario

--- testes/Pets_server/src/test/java/StepsCucumber/LoginVeterinario.java
+++ testes/Pets_server/src/test/java/StepsCucumber/LoginVeterinario.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.SendKeysAction;
 
 import cucumber.api.java.After;
 import cucumber.api.java.pt.*;
